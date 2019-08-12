92e6f9a9f874db52a237f07a1c69ca01d3fd190f
Pietro <arsipietro@gmail.com>
2019-08-12 18:08:50 +0200

    ...

--- progetto.c
+++ progetto.c
@@ -15,13 +15,15 @@
 
 //oppure
 
-//faccio un array 
+//Tengo traccia delle entita' in una hash table. I tipi di relazioni sono in una lista in ordine alfabetico
+//Ogni tipo di relazione ha un BST ordinato con to e from
 
 //struct
 
 typedef struct relation{
-    char rel_name[1000];
+    char rel_name[100];
     struct relation *next;
+    struct relation_tree *tree;
 }relation;
 
 typedef struct entity{
@@ -29,13 +31,25 @@ typedef struct entity{
     struct entity *next;
 }entity;
 
+typedef struct relation_tree{
+    char from[100];
+    char to[100];
+    int relations;
+    struct relation_tree *p;
+    struct relation_tree *left;
+    struct relation_tree *right;
+    struct relation_tree *root;
+}relation_tree;
+
 //prototypes
 void addent_f(char name[], entity* ht_entities[] );
 void delent_f(char name[], entity* ht_entities[]);
-void addrel_f(char name1[], char name2[], char rel[], relation *rel_list);
-void delrel_f(char name1[], char name2[], char rel[], relation *rel_list);
+void addrel_f(char name1[], char name2[], char rel[], relation *rel_list, entity* ht_entities[]);
+void addrel_f2(char name1[], char name2[], char rel[], relation *rel_list, entity* ht_entities[]);
+void delrel_f(char name1[], char name2[], char rel[], relation *rel_list, entity* ht_entities[]);
 void report_f();
-void check_ent_hash(char name[], entity* ht_entities[]);
+int check_ent_hash(char name[], entity* ht_entities[]);
+int hash_function_entities(char name[]);
 
 int main(){
 
@@ -77,13 +91,13 @@ int main(){
             scanf("%s", param1);
             scanf("%s", param2);
             scanf("%s", param3);
-            addrel_f(param1, param2, param3, &rel);
+            addrel_f(param1, param2, param3, &rel, ht_entities);
         }
         else if(strcmp(input_text, "delrel")==0){
             scanf("%s", param1);
             scanf("%s", param2);
             scanf("%s", param3);
-            delrel_f(param1, param2, param3, &rel);
+            delrel_f(param1, param2, param3, &rel, ht_entities);
         }
         else if(strcmp(input_text, "report")==0){
             report_f();
@@ -97,19 +111,87 @@ void addent_f(char name[], entity* ht_entities[]){
     //uso un'hash function per aggiungere le entita' alla hash table
     //come hash function uso      k mod 2053
 
-    //alloco l'elemento da mettere, da mettere poi dentro cosi' alloco solo se strettamente necessario
+    //alloco l'elemento da mettere
     entity * newent;
     newent=malloc(sizeof(entity));
     strcpy(newent->name,name);
 
-    char ent_val_str[301];
+    int hashval;
+
+    // //hash function
+    // //---------------------------------------------------------------------------------------------------------------
+    // char ent_val_str[301];
+    // char transformed[12];
+    // int hashval;
+    
+    // strcpy(ent_val_str,"");
+
+    // for(int i=0;i<strlen(name);i++){
+    //     //converto ogni carattere del nome nel suo valore ascii
+    //     sprintf(transformed,"%d",(int)name[i]);
+    //     //ogni valore ascii viene affiancato al valore ascii del carattere prima
+    //     strcat(ent_val_str, transformed);
+    // }
+    // //---------------------------------------------------------------------------------------------------------------
+    // //fine hash function
+
+    ///printf("Il valore hash trovato e': %s\n", ent_val_str);
+
+    entity *temp_cursor;
+    int positioned=0;
+
+    hashval=hash_function_entities(name)%HASHNUM;
+    
+    //una volta trovata la chiave di hash devo mettere l'entita' nella hash table
+    if(ht_entities[hashval]==NULL){
+        //lo slot e' libero
+        newent->next=NULL;
+        ht_entities[hashval]=newent;
+    }
+    else if(ht_entities[hashval]!=NULL){
+        //lo slot e' occupato, devo scorrere la lista per mettere l'entita' in ordine alfabetico
+        temp_cursor=ht_entities[hashval];
+        do{
+            if(strcmp(temp_cursor->name,newent->name)<0){
+                //l'entita' ha id piu' grande di quella nella lista
+                if(temp_cursor->next==NULL){
+                    //sono all'ultimo elemento
+                    temp_cursor->next=newent;
+                    newent->next=NULL;
+                    positioned=1;
+                }
+                else if(temp_cursor->next!=NULL && strcmp(temp_cursor->next->name,newent->name)>0){
+                    //ho trovato dove metterlo
+                    newent->next=temp_cursor->next;
+                    temp_cursor->next=newent;
+                    positioned=1;
+                }
+            }
+            if(strcmp(temp_cursor->name,newent->name)==0){
+                //se l'entita' esiste gia' allora esco senza fare nulla
+                free(newent);
+                positioned=1;
+            }
+            if(temp_cursor->next==NULL){
+                //sono all'ultimo elemento
+                temp_cursor->next=newent;
+                newent->next=NULL;
+                positioned=1;
+            }
+            //vado avanti solo se esiste l'elemento dopo
+            if(temp_cursor->next!=NULL){
+                temp_cursor=temp_cursor->next;
+            }
+            //printf("pizza");
+        }while(/* temp_cursor->next!=NULL &&  */positioned!=1);
+    }
 }
 
 void delent_f(char name[], entity* ht_entities[]){
     
 }
 
-void addrel_f(char name1[], char name2[], char rel[], relation* rel_list){
+void addrel_f(char name1[], char name2[], char rel[], relation* rel_list, entity* ht_entities[]){
 
     //alloco la nuova relazione da mettere nella lista
     relation * newrel;
@@ -142,23 +224,102 @@ void addrel_f(char name1[], char name2[], char rel[], relation* rel_list){
                 }
                 else if(temp_cursor->next!=NULL && strcmp(temp_cursor->next->rel_name,newrel->rel_name)>0){
                     //ho trovato dove metterlo ma devo verificare di star monitorando entrambe le entita'
-                    newrel->next=temp_cursor->next;
-                    temp_cursor->next=newrel;
-                    positioned=1;
+                    if(check_ent_hash(name1, ht_entities)==1){
+                        if(check_ent_hash(name2, ht_entities)==1){
+                            newrel->next=temp_cursor->next;
+                            temp_cursor->next=newrel;
+                            positioned=1;
+                        }
+                    }
                 }
             }
             if(strcmp(temp_cursor->rel_name,newrel->rel_name)==0){
-                //se la relazione esiste gia' allora esco senza fare nulla
+                //se la relazione esiste gia' allora esco senza fare nulla e libero lo spazio
+                free(newrel);
                 positioned=1;
+            }
+            if(temp_cursor->next==NULL && positioned!=1){
+                //sono all'ultimo elemento
+                temp_cursor->next=newrel;
+                newrel->next=NULL;
+                positioned=1;
+            }
+            if(temp_cursor->next!=NULL && positioned!=1){
+                temp_cursor=temp_cursor->next;
             }
         }while(/* temp_cursor->next!=NULL &&  */positioned!=1);
     }
 }
 
-void delrel_f(char name1[], char name2[], char rel[], relation* rel_list){
+void delrel_f(char name1[], char name2[], char rel[], relation* rel_list, entity* ht_entities[]){
     
 }
 
 void report_f(){
     
+}
+
+int check_ent_hash(char name_to_check[], entity* ht_entities[]){
+    
+    // //hash function
+    // //---------------------------------------------------------------------------------------------------------------
+    // char ent_val_str[301];
+    // char transformed[12];
+    // int hashval;
+    
+    // for(int i=0;i<strlen(name_to_check);i++){
+    //     //converto ogni carattere del nome nel suo valore ascii
+    //     sprintf(transformed,"%d",(int)name_to_check[i]);
+    //     //ogni valore ascii viene affiancato al valore ascii del carattere prima
+    //     strcat(ent_val_str, transformed);
+    // }
+    // //---------------------------------------------------------------------------------------------------------------
+    // //fine hash function
+
+    int hashval;
+    hashval=hash_function_entities(name_to_check)%HASHNUM;
+
+    entity * temp_cursor_ht=ht_entities[hashval];
+
+    if(temp_cursor_ht==NULL){
+        //slot vuoto
+        return 0;
+    }
+    else{
+        do{
+            if(strcmp(temp_cursor_ht->name, name_to_check)==0){
+                //esiste
+                return 1;
+            }
+            temp_cursor_ht=temp_cursor_ht->next;
+        }while(temp_cursor_ht->next!=NULL);
+    }
+
+    return 0;
+}
+
+int hash_function_entities(char name_to_hash[]){
+    //hash function
+    //---------------------------------------------------------------------------------------------------------------
+    char ent_val_str[301]="";
+    char transformed[12];
+
+    //QUI C'E' OVERFLOW
+    
+    for(int i=0;i<strlen(name_to_hash);i++){
+        //converto ogni carattere del nome nel suo valore ascii
+        sprintf(transformed,"%d",(int)name_to_hash[i]);
+        //ogni valore ascii viene affiancato al valore ascii del carattere prima
+        strcat(ent_val_str, transformed);
+    }
+    //---------------------------------------------------------------------------------------------------------------
+    //fine hash function
+
+    int valore=((int)ent_val_str)%HASHNUM;
+
+    if(valore<0){
+        valore=-valore;
+    }
+
+    return valore;
 }
